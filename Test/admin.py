17fa2061818301744153e96faf668386895f9322
parsa <parsa_darvishi@yahoo.com>
2021-08-26 11:15:47 +0430

    added files to git

---
+++ Test/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Test.models import Movie
+# Register your models here.
+
+
+admin.site.register(Movie)
---
+++ Test/url.py
@@ -0,0 +1,9 @@
+
+from django.urls import path, include
+from Test.views import movie_list, movie_detail
+
+urlpatterns = [
+    path('list/', movie_list, name='movie-list'),
+    path('<int:pk>', movie_detail, name='movie_detail')
+]
+
