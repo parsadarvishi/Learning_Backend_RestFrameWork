2f3a29b086cec7fb969da20287e79adb4d8ee821
parsa <parsa_darvishi@yahoo.com>
2021-08-29 10:09:38 +0430

    added files to git2

--- Test/url.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path, include
-from Test.views import movie_list, movie_detail
-
-urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_detail, name='movie_detail')
-]
-
